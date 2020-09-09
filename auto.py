2d27c68a659741c930d97615925fac88c2d8c93f
Syed Aun Muhammad <aunsyedshah@studentpartner.com>
2020-09-09 18:52:18 +0500

    time updated

--- auto.py
+++ auto.py
@@ -12,7 +12,7 @@ username_textbox.send_keys(username)
 
 password_textbox = driver.find_element_by_id("txtPassword")
 password_textbox.send_keys(password)
-time.sleep(10)
+time.sleep(1)
 
 login_button = driver.find_element_by_id("ibtnLogin")
 login_button.click()
